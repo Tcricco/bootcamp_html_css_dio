f65c1d3f7e3753f73d4f92651b5951e6123a9545
Tcricco <tcricco@gmail.com>
2021-02-18 17:00:33 -0300

    Inicio do Desafio do Jogo da Cobrinha

--- Aula JavaScript/js/main.js
+++ Aula JavaScript/js/main.js
@@ -1,1 +1,54 @@
-unsupported reparse point
+/*var nome="Thiago Cordeiro Ricco";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem a idade de "+ idade + " anos.");
+
+// comentario de uma linha
+/* cometario de bloco*/
+
+//alert(idade+idade2);
+
+//f12 - console
+/*
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"));
+alert(frase.toUpperCase("Japão","Brasil"));
+alert(frase.toLowerCase("Japão","Brasil"));
+
+var lista = ["maça","pera","laranja"];
+lista.push("uva");
+lista.pop();
+alert(lista.toString());
+alert(lista.join(" - "));
+
+var frutas = [{nome:"Maçã", cor:"Vermelha"},{nome:"Uva", cor:"Tinta"}];
+alert(frutas[1].nome + " " + frutas[1].cor);
+
+var idade = prompt("Qual a sua idade?")
+
+if(idade>=18){
+    alert("MAIOR")
+}else{
+    alert("menor");
+}
+
+var count=0;
+
+while(count<=5){
+    alert(count);
+    count++;
+}
+var count;
+
+for(count=0;count<=5;count++){
+    alert(count);
+}*/
+
+
+var d = new Date();
+
+alert(d.getMonth());
+alert(d.getDay());
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-unsupported reparse point
+# Repositório de Estudos
+
+## Bootcamp HTML Web Developer 
+
+DIO - Digital Innovation One - https://digitalinnovation.one/
+
+Bootcamp iniciado em 19/01/2021
+
+
+
